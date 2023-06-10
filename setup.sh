4e99ef8ecb42874f5c1ba320b83b97932969be9b
T0dCNg <resetbackbook@gmail.com>
2023-06-10 15:38:01 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update and upgrade packages
+sudo apt update && sudo apt upgrade -y
+
+# Install necessary packages
+sudo apt install docker samba docker.io docker-compose -y
+
+# Run Portainer agent container
+sudo docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:2.18.3
